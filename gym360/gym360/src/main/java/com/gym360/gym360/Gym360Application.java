5b08cf100ff44eda2745e5e49b3bd64ecc272eb6
xgarciaber <xgarciaber@educacion.navarra.es>
2025-10-20 18:28:23 +0200

    Configuración inicial spring boot

---
+++ gym360/gym360/src/main/java/com/gym360/gym360/Gym360Application.java
@@ -0,0 +1,15 @@
+package com.gym360.gym360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gym360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gym360Application.class, args);
+
+
+	}
+
+}
---
+++ gym360/gym360/src/test/java/com/gym360/gym360/Gym360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym360.gym360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gym360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
